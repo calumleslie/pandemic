eec18c57453dcbcb32b194bd71bcf69ebe91e96c
Calum Leslie <calumleslie@gmail.com>
2020-03-15 21:35:00 +0000

    Move people into their own file
    Given all the hardship they're facing here it seems only fair.

--- src/index.ts
+++ src/index.ts
@@ -1,124 +1,34 @@
 import * as d3 from "d3";
 import { Vector } from "./vector";
+import { Person, collide } from "./person";
 
 const canvas = d3.select("svg#canvas");
 
-const CANVAS_SIZE = 300;
+const CANVAS_EXTENT = new Vector(300, 300);
 const PATIENT_COUNT = 100;
-const TICK_MAGNITUDE = 1;
 const COLLISION_DISTANCE = 4;
-const INFECTION_TIME = CANVAS_SIZE / 1;
+// Infection lasts as long as walking width of field
+const INFECTION_TIME = CANVAS_EXTENT.x;
 
-const chooseCoord = d3.randomUniform(CANVAS_SIZE);
+const chooseX = d3.randomUniform(CANVAS_EXTENT.x);
+const chooseY = d3.randomUniform(CANVAS_EXTENT.y);
 const chooseAngle = d3.randomUniform(2 * Math.PI);
 
-enum State {
-    New,
-    Infected,
-    Immune,
-}
-
-function stateColour(state: State) {
-    switch (state) {
-        case State.New:
-            return "#82aaff";
-        case State.Infected:
-            return "#ff8a82";
-        case State.Immune:
-            return "#b082ff";
-    }
-}
-
-class Person {
-    state: State = State.New;
-    position: Vector;
-    direction: Vector;
-
-    private timeInState = 0;
-
-    constructor(position: Vector, direction: Vector) {
-        this.position = position;
-        this.direction = direction;
-    }
-
-    static random() {
-        return new Person(
-            new Vector(chooseCoord(), chooseCoord()),
-            Vector.unitInDirection(chooseAngle()));
-    }
-
-    infect() {
-        if (this.state === State.New) {
-            this.setState(State.Infected);
-        }
-    }
-
-    color() {
-        return stateColour(this.state);
-    }
-
-    bounceOff(other: Person) {
-        this.direction = this.direction.minus(other.direction).normalize();
-    }
-
-    distanceTo(other: Person) {
-        return this.position.minus(other.position).magnitude();
-    }
-
-    update() {
-        this.updateState();
-        this.updatePosition();
-    }
-
-    private updatePosition() {
-        const move = this.direction.times(TICK_MAGNITUDE);
-        this.position = this.position.plus(move);
-
-        // Bounce off the sides. I'm sure this sort of thing would
-        // horrify a game developer but we're not moving far each
-        // tick so we don't need to try hard.
-        if (this.position.x < 0 || this.position.x > CANVAS_SIZE) {
-            this.direction = this.direction.reverseX();
-        }
-
-        if (this.position.y < 0 || this.position.y > CANVAS_SIZE) {
-            this.direction = this.direction.reverseY();
-        }
-    }
-
-    private setState(state: State) {
-        this.timeInState = 0;
-        this.state = state;
-    }
-
-    private updateState() {
-        this.timeInState++;
-        if (this.state === State.Infected && this.timeInState >= INFECTION_TIME) {
-            this.setState(State.Immune);
-        }
-    }
-}
-
-function collide(person1: Person, person2: Person) {
-    if (person1.state === State.Infected || person2.state === State.Infected) {
-        // Infecting does nothing to the infected so let's just do both
-        person1.infect();
-        person2.infect();
-    }
-
-    person1.bounceOff(person2);
-    person2.bounceOff(person1);
+function randomPerson() {
+    return new Person(
+        new Vector(chooseX(), chooseY()),
+        Vector.unitInDirection(chooseAngle()));
 }
 
 const people: Person[] = [];
 for (let i = 0; i < PATIENT_COUNT; i++) {
-    people.push(Person.random());
+    people.push(randomPerson());
 }
 
 people[0].infect();
 
 function updatePeople() {
-    people.forEach(p => p.update());
+    people.forEach(p => p.update(INFECTION_TIME, CANVAS_EXTENT));
 
     for (let i = 0; i < people.length; i++) {
         for (let j = i + 1; j < people.length; j++) {
---
+++ src/person.ts
@@ -0,0 +1,95 @@
+import { Vector } from "./vector";
+
+const TICK_MAGNITUDE = 1;
+
+export enum State {
+    New,
+    Infected,
+    Immune,
+}
+
+export function stateColour(state: State) {
+    switch (state) {
+        case State.New:
+            return "#82aaff";
+        case State.Infected:
+            return "#ff8a82";
+        case State.Immune:
+            return "#b082ff";
+    }
+}
+
+export class Person {
+    state: State = State.New;
+    position: Vector;
+    direction: Vector;
+
+    private timeInState = 0;
+
+    constructor(position: Vector, direction: Vector) {
+        this.position = position;
+        this.direction = direction;
+    }
+
+    infect() {
+        if (this.state === State.New) {
+            this.setState(State.Infected);
+        }
+    }
+
+    color() {
+        return stateColour(this.state);
+    }
+
+    bounceOff(other: Person) {
+        this.direction = this.direction.minus(other.direction).normalize();
+    }
+
+    distanceTo(other: Person) {
+        return this.position.minus(other.position).magnitude();
+    }
+
+    update(infectionTime: number, canvasExtent: Vector) {
+        this.updateState(infectionTime);
+        this.updatePosition(canvasExtent);
+    }
+
+    private updatePosition(canvasExtent: Vector) {
+        const move = this.direction.times(TICK_MAGNITUDE);
+        this.position = this.position.plus(move);
+
+        // Bounce off the sides. I'm sure this sort of thing would
+        // horrify a game developer but we're not moving far each
+        // tick so we don't need to try hard.
+        if (this.position.x < 0 || this.position.x > canvasExtent.x) {
+            this.direction = this.direction.reverseX();
+        }
+
+        if (this.position.y < 0 || this.position.y > canvasExtent.y) {
+            this.direction = this.direction.reverseY();
+        }
+    }
+
+    private setState(state: State) {
+        this.timeInState = 0;
+        this.state = state;
+    }
+
+    private updateState(infectionTime: number) {
+        this.timeInState++;
+        if (this.state === State.Infected && this.timeInState >= infectionTime) {
+            this.setState(State.Immune);
+        }
+    }
+}
+
+export function collide(person1: Person, person2: Person) {
+    if (person1.state === State.Infected || person2.state === State.Infected) {
+        // Infecting does nothing to the infected so let's just do both
+        person1.infect();
+        person2.infect();
+    }
+
+    person1.bounceOff(person2);
+    person2.bounceOff(person1);
+}
